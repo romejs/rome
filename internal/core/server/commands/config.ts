9e3b991e319fed8e40f9706f05bcd3fbd30faa0d
JustBeYou <mihailferaru2000@gmail.com>
2021-07-07 15:49:31 +0300

    draft config pop/remove

--- internal/core/server/commands/config.ts
+++ internal/core/server/commands/config.ts
@@ -36,22 +36,29 @@ function defineFlags(c: Consumer): Flags {
 	};
 }
 
+type Action = "push" | "set" | "location" | "enable" | "disable" | "remove" | "pop" | "setDirectory"
+
 async function runCommand(
 	req: ServerRequest,
 	flags: Flags,
 	value: boolean | string | (string[]),
-	action: string,
+	action: Action,
 ) {
 	const {reporter} = req;
-	const [keyParts] = req.query.args;
+	const [keyPath] = req.query.args;
 
 	function modify(consumer: Consumer) {
 		// Set the specified value
 		let keyConsumer = consumer;
-		for (const key of keyParts.split(".")) {
+		let prevKeyConsumer = consumer;
+		const keyParts = keyPath.split(".");
+
+		// We are sure that keyParts.length is >= 1
+		for (const key of keyParts) {
 			if (!keyConsumer.exists()) {
 				keyConsumer.setValue({});
 			}
+			prevKeyConsumer = keyConsumer;
 			keyConsumer = keyConsumer.get(key);
 		}
 
@@ -63,6 +70,16 @@ async function runCommand(
 				),
 				...(Array.isArray(value) ? value : []),
 			]);
+		} else if (action === "remove") {
+			const lastKey = keyParts[keyParts.length-1];
+			prevKeyConsumer.delete(lastKey);
+		} else if (action === "pop") {
+			const existing = Array.from(
+				keyConsumer.required([]).asIterable(),
+				(c) => c.asUnknown(),
+			);
+			const toRemove = Array.isArray(value) ? value : [];
+			keyConsumer.setValue(existing.filter(item => typeof item !== "string" || !toRemove.includes(item)));
 		} else {
 			keyConsumer.setValue(value);
 		}
@@ -78,18 +95,32 @@ async function runCommand(
 			return;
 		}
 
-		reporter.success(
-			markup`${action === "push" ? "Adding" : "Setting"} <emphasis>${prettyFormat(
-				value,
-			)}</emphasis> to <emphasis>${keyParts}</emphasis> in the config <emphasis>${configPath}</emphasis>`,
-		);
+
+		switch (action) {
+			case "remove":
+				reporter.success(markup`Removing <emphasis>${keyPath}</emphasis> in the config <emphasis>${configPath}</emphasis>`);
+				break;
+			case "pop":
+				reporter.success(
+					markup`Removing <emphasis>${prettyFormat(
+						value,
+					)}</emphasis> from <emphasis>${keyPath}</emphasis> in the config <emphasis>${configPath}</emphasis>`,
+				); 
+				break;
+			default:
+				reporter.success(
+					markup`${action === "push" ? "Adding" : "Setting"} <emphasis>${prettyFormat(
+						value,
+					)}</emphasis> to <emphasis>${keyPath}</emphasis> in the config <emphasis>${configPath}</emphasis>`,
+				);
+		}
 
 		if (value === "true" || value === "false") {
 			const suggestedCommand = value === "true" ? "enable" : "disable";
 			reporter.warn(
 				markup`Value is the string <emphasis>${value}</emphasis> but it looks like a boolean. You probably meant to use the command:`,
 			);
-			reporter.command(`config ${suggestedCommand} ${keyParts}`);
+			reporter.command(`config ${suggestedCommand} ${keyPath}`);
 		}
 
 		// Load the config file again
@@ -245,7 +276,6 @@ export const setDirectory = createServerCommand<Flags>({
 
 			value = cwd.relative(path).join();
 		}
-
 		await runCommand(req, flags, value, "setDirectory");
 	},
 });
@@ -272,4 +302,28 @@ export const push = createServerCommand<Flags>({
 		req.expectArgumentLength(2, Infinity);
 		await runCommand(req, flags, req.query.args.slice(1), "push");
 	},
+});
+
+export const remove = createServerCommand<Flags>({
+	category: commandCategories.PROCESS_MANAGEMENT,
+	description: markup`modify a project config - remove ${"<key>"}`,
+	usage: "<key>",
+	examples: [],
+	defineFlags,
+	async callback(req, flags) {
+		req.expectArgumentLength(1);
+		await runCommand(req, flags, req.query.args[1], "remove");
+	}
+});
+
+export const pop = createServerCommand<Flags>({
+	category: commandCategories.PROCESS_MANAGEMENT,
+	description: markup`modify a project config - remove ${"<values>"} from ${"<key>"}`,
+	usage: "<key> <...values>",
+	examples: [],
+	defineFlags,
+	async callback(req, flags) {
+		req.expectArgumentLength(2, Infinity);
+		await runCommand(req, flags, req.query.args.slice(1), "pop");
+	}
 });
